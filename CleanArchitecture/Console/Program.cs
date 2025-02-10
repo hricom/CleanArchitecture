a9cdd3fdfea842a5e62f98416db67f13eed8e9e9
HRM <hricoma@eafit.edu.co>
2025-02-09 23:20:31 -0500

    schema add

---
+++ CleanArchitecture/Console/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello, World!");
+            System.Console.ReadLine();
+        }
+    }
+}
